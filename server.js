f59473fc8fcf8863fbd0cc1b69589196fc28cf08
Mehdi Naderi Varandi <MehdiNV@hotmail.com>
2019-11-23 22:15:52 +0000

    serverjs modification

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
-
+var cors = require('cors');
 var mongoose   = require('mongoose');
 
 // SCHEMAS
@@ -20,20 +20,16 @@ const CONNECTION_STRING = 'mongodb://user-1:i-hate-passwords@yunus-finance-clust
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 mongoose.connect(CONNECTION_STRING);
 
-var port = process.env.PORT || 8080;        // set our port
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
 
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'Accessed API!' });   
-});
-
 
 // more routes for our API will happen here
 
@@ -43,7 +39,7 @@ router.route('/customers')
     .post(function(req, res) {
 
         var customer = new Customer();      // create a new instance of the Customer model
-        
+
         // setting the customer's attributes to those passed in by the request body
 
         customer.firstName = req.body.firstName;
@@ -72,5 +68,6 @@ app.use('/api', router);
 
 // START THE SERVER
 // =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
