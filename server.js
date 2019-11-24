854f3d23e76c326361ae53ffe5cb8b9edfc1fbd6
sophie-norman <s.norman2@newcastle.ac.uk>
2019-11-24 06:20:07 +0000

    refactor code

--- server.js
+++ server.js
@@ -19,6 +19,85 @@ const CONNECTION_STRING = 'mongodb+srv://user-1:i-hate-passwords@yunus-finance-c
 
 console.log('I just called, to say, I love youuuuu');
 
+// FUNCTIONS FOR API
+
+var getCustomers = (req, res) => {
+    Customer.find((err, customers) => {
+        if (err)
+            res.send(err);
+
+        res.json(customers);
+    })
+}
+
+var createCustomer = (req, res) => {
+    var customer = new Customer({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        dateOfBirth: req.body.dateOfBirth,
+        phoneNumberCountryCode: req.body.phoneNumberCountryCode,
+        phoneNumber: req.body.phoneNumber,
+        countryName: req.body.countryName,
+        nativeLanguage: req.body.nativeLanguage
+    });      // create a new instance of the Customer model
+
+    // save the bear and check for errors
+    customer.save(function(err) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json({success: true, customer: customer });
+        }
+    });
+}
+
+var getCustomer = (req, res) => {
+    Customer.findById(req.params.cust_id, (err, customer) => {
+        if (err) {
+            res.send({sucess: false, response: err});
+        } else if (!customer) {
+            res.send({sucess: false, response: 'No customer found'});
+        } else {
+            res.json({sucess: true, response: customer});
+        }
+    })
+}
+
+var deleteCustomer = (req, res) => {
+    Customer.deleteOne({_id: req.params.cust_id}, (err, customer) => {
+        if (err)
+            res.send(err);
+        
+        res.json({message: 'successfully deleted customer'});
+    })
+}
+
+var updateCustomer = (req, res) => {
+    Customer.findById(req.params.cust_id, (err, customer) => {
+        if (err) {
+            res.send({sucess: false, response: err});
+        } else if (!customer) {
+            res.send({sucess: false, response: 'No customer found'});
+        } else {
+            customer.firstName = req.body.firstName;
+            customer.lastName = req.body.lastName;
+            customer.email = req.body.email;
+            customer.dateOfBirth = req.body.dateOfBirth;
+            customer.phoneNumberCountryCode = req.body.phoneNumberCountryCode;
+            customer.phoneNumber = req.body.phoneNumber;
+            customer.countryName = req.body.countryName;
+            customer.nativeLanguage = req.body.nativeLanguage;
+
+            customer.save((err) => {
+                if (err)
+                    res.send({success: false});
+            })
+            res.json({sucess: true, response: customer});
+        }
+    })
+}
+
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -43,86 +122,27 @@ app.get('/tabs/tab1', (req,res) => {
 
 
 router.route('/customers')
-
-    .get(function(req, res) {
-        Customer.find((err, customers) => {
-            if (err)
-                res.send(err);
-
-            res.json(customers);
-        })
-    })
-
-    // create a customer (accessed at POST http://localhost:8080/api/customers)
-    .post(function(req, res) {
-
-        var customer = new Customer({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            dateOfBirth: req.body.dateOfBirth,
-            phoneNumberCountryCode: req.body.phoneNumberCountryCode,
-            phoneNumber: req.body.phoneNumber,
-            countryName: req.body.countryName,
-            nativeLanguage: req.body.nativeLanguage
-        });      // create a new instance of the Customer model
-
-        // save the bear and check for errors
-        customer.save(function(err) {
-            if (err) {
-                res.send(err);
-            } else {
-                res.json({success: true, customer: customer });
-            }
-        });
-
-    });
+    .get(getCustomers)
+    .post(createCustomer);
 
 router.route('/customers/:cust_id')
-    .get((req, res) => {
-        Customer.findById(req.params.cust_id, (err, customer) => {
-            if (err) {
-                res.send({sucess: false, response: err});
-            } else if (!customer) {
-                res.send({sucess: false, response: 'No customer found'});
-            } else {
-                res.json({sucess: true, response: customer});
-            }
-        })
-    })
+    .get(getCustomer)
+    .delete(deleteCustomer)
+    .put(updateCustomer);
 
-    .delete((req, res) => {
-        Customer.deleteOne({_id: req.params.cust_id}, (err, customer) => {
-            if (err)
-                res.send(err);
-            
-            res.json({message: 'successfully deleted customer'});
-        })
-    })
+// CAPITAL ONE INTERACTIONS
 
-    .put((req, res) => {
-        Customer.findById(req.params.cust_id, (err, customer) => {
-            if (err) {
-                res.send({sucess: false, response: err});
-            } else if (!customer) {
-                res.send({sucess: false, response: 'No customer found'});
-            } else {
-                customer.firstName = req.body.firstName;
-                customer.lastName = req.body.lastName;
-                customer.email = req.body.email;
-                customer.dateOfBirth = req.body.dateOfBirth;
-                customer.phoneNumberCountryCode = req.body.phoneNumberCountryCode;
-                customer.phoneNumber = req.body.phoneNumber;
-                customer.countryName = req.body.countryName;
-                customer.nativeLanguage = req.body.nativeLanguage;
+router.route('/request_loan/:cust_id')
 
-                customer.save((err) => {
-                    if (err)
-                        res.send({success: false});
-                })
-                res.json({sucess: true, response: customer});
-            }
-        })
+    // In the body: loan amount & loan reason
+    .post((req, res) => {
+        // STEP 1: Retrieve customer details
+
+        // STEP 2: Create account with Captial One
+
+        // STEP 3: Store account details 
+
+
     });
 
 
@@ -136,3 +156,4 @@ app.use('/api', router);
 app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
+
